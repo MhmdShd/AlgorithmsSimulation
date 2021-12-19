1f2b044d8b2106dbcd558e6687cdeb6157d76bce
MhmdShd <mhmdalishd10@gmail.com>
2021-12-19 16:37:13 +0200

    Initial commit

---
+++ Alogs.py
@@ -0,0 +1,260 @@
+from time import sleep
+from math import floor
+
+
+#  Heap Sort
+
+def max_heapify(data, i, size, drawData, speed, visual):
+    l = 2 * i + 1
+    r = 2 * i + 2
+    if l <= size - 1 and data[l] > data[i]:
+        large = l
+    else:
+        large = i
+    if r <= size - 1 and data[r] > data[large]:
+        large = r
+    if large != i:
+        data[i], data[large] = data[large], data[i]
+        max_heapify(data, large, size, drawData, speed, visual)
+
+    # drawing if user chose to
+    if visual == True:
+        drawData(data, getColorheap(len(data), i, large, l, r))
+
+    sleep(speed)
+
+def build_max_heap(data, drawData, speed, visual):
+    size = len(data)
+    for i in range(floor(len(data) / 2) - 1, -1, -1):
+        sleep(speed)
+        max_heapify(data, i, size, drawData, speed, visual)
+def heap_sort(data, drawData, speed, visual=False):
+    build_max_heap(data, drawData, speed, visual)
+    size = len(data)
+    for i in range(len(data) - 1, 0, -1):
+        data[0], data[i] = data[i], data[0]
+        size = size - 1
+        max_heapify(data, 0, size, drawData, speed, visual)
+# getting proper color
+def getColorheap(datalen,I,large,l,r):
+    color = []
+    for i in range(datalen):
+        if i <= datalen:
+            color.append("grey")
+        else:
+            color.append("white")
+        if i == I:
+            color[i] = 'green'
+        if i == l:
+            color[i] = 'blue'
+        if i == r:
+            color[i] = 'yellow'
+    return color
+
+
+
+#  Insertion Sort
+
+def insertion_sort(data,drawData,speed,visual = False):
+    for j in range(1,len(data)):
+        x = data[j]
+        i=j-1
+        while i>=0 and data[i] > x:
+            sleep(speed)
+            data[i+1] = data[i]
+            i-=1
+
+
+            # drawing if user chose to
+            if visual == True:
+                drawData(data, ['green' if x == j or x == i else 'red' for x in range(len(data))])
+
+        data[i+1] = x
+
+
+
+
+
+# Selection Sort
+
+def selection_sort(data, drawData, speed, visual=False):
+    for i in range(0, len(data)):
+        x = data[i]
+        small = i
+        sleep(speed)
+        for j in range(i, len(data)):
+            if data[j] < data[small]:
+
+                # drawing if user chose to
+                if visual == True:
+                    drawData(data, ['green' if x == j or x == i else 'red' for x in range(len(data))])
+
+                sleep(speed)
+                small = j
+        data[i], data[small] = data[small], data[i]
+
+
+
+
+
+# Merge Sort
+
+def merge(data, head, idx, tail, drawData, speed, visual=False):
+    n1 = idx - head + 1
+    n2 = tail - idx
+    left = [0] * len(data)
+    right = [0] * len(data)
+    for i in range(n1):
+        sleep(speed)
+        left[i] = data[head + i]
+
+        # drawing if user chose to
+        if visual == True:
+            drawData(data, getColor(len(data), head, tail))
+
+    for i in range(n2):
+        sleep(speed)
+        right[i] = data[idx + i + 1]
+
+        # drawing if user chose to
+        if visual == True:
+            drawData(data, getColor(len(data), head, tail))
+
+    left[n1] = float('inf')  # setting the value to infinity
+    right[n2] = float('inf')  # setting the value to infinity
+
+    i = 0
+    j = 0
+    for x in range(head, tail + 1):
+
+        # drawing if user chose to
+        if visual == True:
+            drawData(data, getColor(len(data), head, tail))
+
+        if left[i] <= right[j]:
+            data[x] = left[i]
+            i += 1
+        else:
+            data[x] = right[j]
+            j += 1
+def merge_sort(data, head, tail, drawData, speed, visual=False):
+    if head < tail:
+        idx = floor((head + tail) / 2)
+        merge_sort(data, head, idx, drawData, speed, visual)
+        merge_sort(data, idx + 1, tail, drawData, speed, visual)
+        merge(data, head, idx, tail, drawData, speed, visual)
+# getting proper color
+def getColor(datalen, head, tail):
+    color = []
+    for i in range(datalen):
+        if i >= head and i <= tail:
+            color.append("yellow")
+        else:
+            color.append("grey")
+        if i == tail:
+            color[i] = 'blue'
+        if i == head:
+            color[i] = 'blue'
+    return color
+
+
+
+
+# Bubble Sort
+
+def bubble_sort(data, drawData, speed, visual=False):
+    for x in range(len(data) - 1):
+        for j in range(len(data) - 1):
+            if data[j] > data[j + 1]:
+                data[j], data[j + 1] = data[j + 1], data[j]
+
+                # drawing if user chose to
+                if visual == True:
+                    drawData(data, ['green' if x == j or x == j + 1 else 'red' for x in range(len(data))])
+
+                sleep(speed)
+
+
+
+
+# Merge Sort
+
+def partition(data, head, tail, drawData, speed, visual=False):
+    pivot = data[tail]
+    border = head - 1
+
+    # drawing if user chose to
+    if visual == True:
+        drawData(data, getColorquick(len(data), head, tail, border, border))
+
+    sleep(speed)
+    for j in range(head, tail):
+        if data[j] <= pivot:
+
+            # drawing if user chose to
+            if visual == True:
+                drawData(data, getColorquick(len(data), head, tail, border, j, True))
+
+            sleep(speed)
+            border += 1
+            data[border], data[j] = data[j], data[border]
+
+        # drawing if user chose to
+        if visual == True:
+            drawData(data, getColorquick(len(data), head, tail, border, j))
+
+        sleep(speed)
+
+    # drawing if user chose to
+    if visual == True:
+        drawData(data, getColorquick(len(data), head, tail, border, tail, True))
+
+    sleep(speed)
+    data[border + 1], data[tail] = data[tail], data[border + 1]
+
+    return border + 1
+def quick_sort(data, head, tail, drawData, timeTick, visual=False):
+    if head < tail:
+        p = partition(data, head, tail, drawData, timeTick, visual)
+
+        quick_sort(data, head, p - 1, drawData, timeTick, visual)
+        quick_sort(data, p + 1, tail, drawData, timeTick, visual)
+# getting proper color
+def getColorquick(datalen, head, tail, border, currId, isSwapping=False):
+    color = []
+    for i in range(datalen):
+        if i >= head and i <= tail:
+            color.append("grey")
+        else:
+            color.append("white")
+
+        if i == tail:
+            color[i] = 'blue'
+        elif i == border:
+            color[i] = 'red'
+        elif i == currId:
+            color[i] = 'yellow'
+        if isSwapping:
+            if i == border or i == currId:
+                color[i] = 'green'
+    return color
+
+
+
+# Counting Sort
+def counting_sort(data, b, k, drawData, speed, visual=False):
+    c = [0] * (k + 1)
+    for j in range(0, len(data)):
+        x = data[j]
+        c[data[j]] += 1
+    for i in range(1, k + 1):
+        c[i] += c[i - 1]
+    for j in range(len(data) - 1, -1, -1):
+        b[c[data[j]] - 1] = data[j]
+
+        # drawing if user chose to
+        if visual == True:
+            drawData(b, ['green' if x == b[j] else 'blue' for x in range(len(data))])
+
+        sleep(speed)
+        c[data[j]] -= 1
---
+++ Project.py
@@ -0,0 +1,342 @@
+
+
+#  Friendly Program for some Sorting Algorithms Visualization
+
+#  Written by Mhmd Ali
+#  Discord: Andr0x#8929
+#  Github: https://github.com/MhmdShd
+
+
+
+
+from time import perf_counter_ns
+from tkinter import *
+from tkinter import ttk
+import random
+import multiprocessing as mp
+
+
+#alog files
+# from quicksort import quick_sort
+# from selectionsort import selection_sort
+# from bubblesort import bubble_sort
+# from insertionsort import insertion_sort
+# from mergesort import merge_sort
+# from maxheap import heap_sort
+# from countingsort import counting_sort
+from Alogs import *
+
+file = open('Simulationn-algo.txt','w')  # open Simulation file to write data
+
+
+root = Tk()   # Application initialization
+
+
+root.title('sorting')   # Application Name
+
+
+root.maxsize(1800,1150)    # Application dimention
+
+
+root.config(bg = 'black')   # Applicatino Background
+
+
+data = []  # main array
+
+
+size = 0  # size of the array for simulation
+
+
+
+# Drawing Function for visualisation
+
+def drawData(data,color):
+
+    # canvas settings
+    canvas.delete('all')
+    c_height = 830
+    c_width = 1800
+
+    # var width
+    x_width = c_width / (len(data)+1)
+
+    for i, height in enumerate(data):
+
+        # left point of rectangle
+        x0 = i * x_width
+        y0 = c_height - height
+
+        # right point of rectangle
+        x1 = (i + 1) * x_width
+        y1 = c_height
+
+        # draws rectangle
+        canvas.create_rectangle(x0,y0,x1,y1,fill = color[i])
+
+    #update screen
+    root.update()
+
+
+# Generate random Array values ( rectangle height )
+def Generate():
+    global data
+    minval = int(MinEntry.get())
+    maxval = int(MaxEntry.get())
+    size = int(sizeEntry.get())
+    if minval > maxval : minval,maxval=maxval,minval
+    if minval<0:minval=0
+    if maxval>830:maxval=830
+    if size < 3 : size = 3
+    data = []
+    for x in range(size):
+        data.append((random.randrange(minval,maxval+1)))
+    drawData(data,['red' for x in range(len(data))])
+
+
+def simulate_quick():
+    end = 0
+    global data
+    Data = data
+    for i in range(500):
+        start = perf_counter_ns()
+        quick_sort(Data, 0, size - 1, drawData, 0)
+        end += (perf_counter_ns() - start)/(10**6)
+    file.write(f'\n     Quick Sort : {end/500} ms')
+    print(f'Quick Sort for n = {size} DONE!! {end/500} ms')
+def simulate_bubble():
+    end = 0
+    global data
+    Data = data
+    for i in range(500):
+        start = perf_counter_ns()
+        bubble_sort(Data,drawData,0)
+        end += (perf_counter_ns() - start)/(10**6)
+    file.write(f'\n     Bubble Sort : {end/500} ms')
+    print(f'Bubble Sort for n = {size} DONE!! {end/500} ms')
+def simulate_insertion():
+    end = 0
+    global data
+    Data = data
+    for i in range(500):
+        start = perf_counter_ns()
+        insertion_sort(Data,drawData,0)
+        end += (perf_counter_ns() - start)/(10**6)
+    file.write(f'\n     Insertion Sort : {end/500} ms')
+    print(f'Insertion Sort for n = {size} DONE!! {end/500} ms')
+def simulate_selection():
+    end = 0
+    global data
+    Data = data
+    for i in range(500):
+        start = perf_counter_ns()
+        selection_sort(Data,drawData,0)
+        end += (perf_counter_ns() - start)/(10**6)
+    file.write(f'\n     Selection Sort : {end/500} ms')
+    print(f'Selection Sort for n = {size} DONE!! {end/500} ms')
+def simulate_merge():
+    end = 0
+    global data
+    Data = data
+    for i in range(500):
+        start = perf_counter_ns()
+        merge_sort(Data,0,size-1,drawData,0)
+        end += (perf_counter_ns() - start)/(10**6)
+    file.write(f'\n     Merge Sort : {end/500} ms')
+    print(f'Merge Sort for n = {size} DONE!! {end/500} ms')
+def simulate_heap():
+    end = 0
+    global data
+    Data = data
+    for i in range(500):
+        start = perf_counter_ns()
+        heap_sort(Data,drawData,0)
+        end += (perf_counter_ns() - start)/(10**6)
+    file.write(f'\n     Heap Sort : {end/500} ms')
+    print(f'Heap Sort for n = {size} DONE!! {end/500} ms')
+def simulate_counting():
+    end = 0
+    b=[0]*size
+    max = 0
+    global data
+    Data = data
+    for i in range(len(Data)):
+        if max < Data[i]: max = Data[i]
+    for i in range(500):
+        start = perf_counter_ns()
+        counting_sort(Data,b,max,drawData,0)
+        end += (perf_counter_ns() - start) / (10 ** 6)
+    file.write(f'\n     Counting Sort : {end / 500} ms')
+    print(f'Counting Sort for n = {size} DONE!! {end/500} ms')
+
+
+
+# simulation function
+def Simulate():
+    global size
+    size = 50
+    MinEntry.delete(0,'end')
+    MinEntry.insert(0,'1')
+    MaxEntry.delete(0,'end')
+    MaxEntry.insert(0,'830')
+    speedScale.set(0)
+    visual.set(0)
+    while size <= 500:
+        sizeEntry.delete(0,'end')
+        sizeEntry.insert(0,size)
+        Generate()
+        file.write(f'\nSize = {size} : ')
+        mp.Process(target=simulate_heap).run()
+        mp.Process(target=simulate_counting).run()
+        mp.Process(target=simulate_merge).run()
+        mp.Process(target=simulate_quick).run()
+        mp.Process(target=simulate_selection).run()
+        mp.Process(target=simulate_bubble).run()
+        mp.Process(target=simulate_insertion).run()
+        size += 10
+
+
+
+# calling algorithm functions
+def startAlgorithm():
+    global data
+
+    # if user didn't generate values yet
+    if not data:
+        return
+
+    # quick sort
+    if (algMenu.get() == 'Quick Sort'):
+        if visual.get() == 1:
+            quick_sort(data,0,len(data)-1,drawData,speedScale.get(),True)
+        else:
+            quick_sort(data,0,len(data)-1,drawData,speedScale.get())
+
+        # coloring all elements green when sorting is finished
+        drawData(data, ['green' for x in range(len(data))])
+
+    # bubble Sort
+    elif algMenu.get() == 'Bubble Sort':
+        if visual.get() == 1:
+            bubble_sort(data, drawData, speedScale.get(),True)
+        else:
+            bubble_sort(data, drawData, speedScale.get())
+        # coloring all elemts green when sorting is complete
+        drawData(data, ['green' for x in range(len(data))])
+
+    # Insertion Sort
+    elif algMenu.get() == 'Insertion Sort':
+        if visual.get() == 1:
+            insertion_sort(data,drawData,speedScale.get(),True)
+        else:
+            insertion_sort(data,drawData,speedScale.get())
+        # coloring all elements green when sorting is finished
+        drawData(data, ['green' for x in range(len(data))])
+
+    # Selection Sort
+    elif algMenu.get() == 'Selection Sort':
+        if visual.get() == True:
+            selection_sort(data,drawData,speedScale.get(),True)
+        else:
+            selection_sort(data,drawData,speedScale.get())
+        # coloring all elements green when sorting is finished
+        drawData(data, ['green' for x in range(len(data))])
+
+    # Merge Sort
+    elif algMenu.get() == 'Merge Sort':
+        if visual.get()==True:
+            merge_sort(data,0,len(data)-1,drawData,speedScale.get(),True)
+        else:
+            merge_sort(data,0,len(data)-1,drawData,speedScale.get())
+
+        # coloring all elements green when sorting is finished
+        drawData(data, ['green' for x in range(len(data))])
+
+    # Heap Sort
+    elif algMenu.get() == 'Heap Sort':
+        if visual.get() == True:
+            heap_sort(data,drawData,speedScale.get(),True)
+        else:
+            heap_sort(data,drawData,speedScale.get())
+
+        # coloring all elements green when sorting is finished
+        drawData(data, ['green' for x in range(len(data))])
+
+    # Counting Sort
+    elif algMenu.get() == 'Counting Sort':
+        b = [0]*len(data)
+
+        max = 0
+        for i in range(len(data)):
+            if max < data[i]:max = data[i]
+        if visual.get() == True:
+            counting_sort(data,b,max,drawData,speedScale.get(),True)
+        else:
+            counting_sort(data,b,max,drawData,speedScale.get())
+
+        # coloring all elements green when sorting is finished
+        drawData(b,['green' for x in range(len(data))])
+
+
+
+# Settings up UI Size
+
+UI_frame = Frame(root,width=1800,height = 300,bg ='grey')
+UI_frame.grid(row = 0,column=0,padx = 10, pady = 5)
+canvas = Canvas(root, width=1800,height = 830,bg = 'white')
+canvas.grid(row = 1,column=0,pady = 5)
+
+
+# row[0]
+
+# Select Algorithm:
+Label(UI_frame,text='Alogorithm: ', bg = "grey").grid(row=0,column=0,padx=5,pady=5,sticky=W)
+algMenu = ttk.Combobox(UI_frame, values = ['Bubble Sort','Heap Sort', 'Insertion Sort', 'Selection Sort', 'Quick Sort', 'Counting Sort','Merge Sort'])
+algMenu.grid(row=0,column=1,padx=5,pady=5)
+algMenu.current(0)
+
+# speed scale
+speedScale = Scale(UI_frame,from_=0,to=5,length=100,digits=2,resolution=0.2,orient = HORIZONTAL,label = 'Select Delay [s]')
+speedScale.grid(row = 0,column=2,padx=5,pady=5)
+
+# visual box ( show step by step )
+visual = Scale(UI_frame,from_=0,to=1,length=100,digits=2,resolution=1,orient = HORIZONTAL,label = 'Visualise? [0/1]')
+visual.grid(row = 0,column=3,padx=5,pady=5)
+
+# start button
+Button(UI_frame,text='Start',command=startAlgorithm,bg='green').grid(row=0,column=4,padx=5,pady=5)
+
+
+Label(UI_frame,text='when clicked, check terminal:',bg = 'grey').grid(row=0,column=5,pady=5,padx=5)
+# simulation button (don't click, it will take alot of time to finish)
+Button(UI_frame,text='Simulate',command=Simulate,bg='red').grid(row=0,column=6,padx=5,pady=5)
+
+
+# row[1]
+
+# select size
+
+Label(UI_frame,text='Size: ', bg = "grey").grid(row=1,column=0,padx=5,pady=5,sticky=W)
+sizeEntry = Entry(UI_frame)
+sizeEntry.grid(row=1,column=1,padx=5,pady=5,sticky = W)
+
+
+# minimume value
+
+Label(UI_frame,text='Min Value: ', bg = "grey").grid(row=1,column=2,padx=5,pady=5,sticky=W)
+MinEntry = Entry(UI_frame)
+MinEntry.grid(row=1,column=3,padx=5,pady=5,sticky = W)
+
+
+# max Value
+
+Label(UI_frame,text='Max Value: ', bg = "grey").grid(row=1,column=4,padx=5,pady=5,sticky=W)
+MaxEntry = Entry(UI_frame)
+MaxEntry.grid(row=1,column=5,padx=5,pady=5,sticky = W)
+
+
+# generate Vutton
+Button(UI_frame,text='Generate',command=Generate,bg='yellow').grid(row=1,column=6,padx=5,pady=5)
+
+
+
+root.mainloop()
